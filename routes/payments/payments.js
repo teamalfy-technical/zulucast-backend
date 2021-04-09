37762fd4defbf375264941289bf43c74158015dd
ibrahimafatima <sakho92iba@gmail.com>
2021-04-09 12:59:13 +0100

    added stripe back

--- routes/payments/payments.js
+++ routes/payments/payments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
-//const stripe = require("stripe")("sk_test_wqL6m08V5M2uSQa9LzePx8dV");
+const stripe = require("stripe")("sk_test_wqL6m08V5M2uSQa9LzePx8dV");
 const fetch = require("node-fetch");
 const config = require("config");
 
@@ -10,66 +10,66 @@ router.get("/", (req, res) => {
   res.send("PAYMENT");
 });
 
-router.post("/", async (req, res) => {
-  // amount, customerObj
-  const body = {
-    tx_ref: uuid(),
-    amount: req.body.amount,
-    currency: "USD",
-    redirect_url: "https://zulucast.herokuapp.com/success",
-    payment_options: "card",
-    meta: {
-      consumer_id: 23,
-      consumer_mac: "92a3-912ba-1192a",
-    },
-    customer: {
-      email: req.body.email,
-      phonenumber: "0205648089",
-      name: "Yemi Desola",
-    },
-    customizations: {
-      title: "ZuluCast Payment",
-      description: "Watch everywhere, at anytime.",
-      logo: "",
-    },
-  };
-
-  const response = await fetch("https://api.flutterwave.com/v3/payments", {
-    method: "POST",
-    headers: {
-      Authorization: config.get("flutterwave_key"),
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-  const val = await response.json();
-  res.send(val.data);
-});
-
 // router.post("/", async (req, res) => {
-//   const { product, token } = req.body;
+//   // amount, customerObj
+//   const body = {
+//     tx_ref: uuid(),
+//     amount: req.body.amount,
+//     currency: "USD",
+//     redirect_url: "https://zulucast.herokuapp.com/success",
+//     payment_options: "card",
+//     meta: {
+//       consumer_id: 23,
+//       consumer_mac: "92a3-912ba-1192a",
+//     },
+//     customer: {
+//       email: req.body.email,
+//       phonenumber: "0205648089",
+//       name: "Yemi Desola",
+//     },
+//     customizations: {
+//       title: "ZuluCast Payment",
+//       description: "Watch everywhere, at anytime.",
+//       logo: "",
+//     },
+//   };
 
-//   const idempotencyKey = uuid();
-
-//   stripe.customers
-//     .create({
-//       email: token.email,
-//       source: token.id,
-//     })
-//     .then((customer) => {
-//       stripe.charges.create(
-//         {
-//           amount: product.price * 100,
-//           currency: "usd",
-//           customer: customer.id,
-//           receipt_email: token.email,
-//           description: `Purchase of ${product.name}`,
-//         },
-//         { idempotencyKey }
-//       );
-//     })
-//     .then((result) => res.status(200).json(result))
-//     .catch((err) => console.log(err));
+//   const response = await fetch("https://api.flutterwave.com/v3/payments", {
+//     method: "POST",
+//     headers: {
+//       Authorization: config.get("flutterwave_key"),
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(body),
+//   });
+//   const val = await response.json();
+//   res.send(val.data);
 // });
+
+router.post("/", async (req, res) => {
+  const { product, token } = req.body;
+
+  const idempotencyKey = uuid();
+
+  stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchase of ${product.name}`,
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;

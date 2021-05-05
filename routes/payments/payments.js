c1b846ea4dd4adb3eb8fbb111bef756616c90d0a
ibrahimafatima <sakho92iba@gmail.com>
2021-05-05 15:39:19 +0100

    vv

--- routes/payments/payments.js
+++ routes/payments/payments.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
     tx_ref: uuid(),
     amount: req.body.amount,
     currency: "USD",
-    redirect_url: config.get("redirect_url"),
+    redirect_url: "https://zulucast.herokuapp.com/success",
     payment_options: "card",
     meta: {
       consumer_id: 23,

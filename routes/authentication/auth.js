f072962388292207915fbb83c91ae39f00c5a6da
ibrahimafatima <sakho92iba@gmail.com>
2021-05-28 13:56:19 +0100

    added new end

--- routes/authentication/auth.js
+++ routes/authentication/auth.js
@@ -368,4 +368,45 @@ router.post(
   }
 );
 
+router.post(
+  "/upload-mobile",
+  [uploader.single("file")],
+  async (req, res, next) => {
+    if (!req.file) {
+      return res.status(400).send("No file uploaded");
+    }
+
+    let user = await Auth.findOne({ email: "alpha@gmail.com" });
+
+    // Create new blob in the bucket referencing the file
+    const blob = bucket.file(req.file.originalname);
+
+    // Create writable stream and specifying file mimetype
+    const blobWriter = blob.createWriteStream({
+      metadata: {
+        contentType: req.file.mimetype,
+      },
+    });
+
+    blobWriter.on("error", (err) => next(err));
+
+    blobWriter.on("finish", async () => {
+      // Assembling public URL for accessing the file via HTTP
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        bucket.name
+      }/o/${encodeURI(blob.name)}?alt=media`;
+
+      // Return the file name and its public URL
+      user.profileURL = publicUrl;
+      await user.save();
+      res
+        .status(200)
+        .send({ fileName: req.file.originalname, fileLocation: publicUrl });
+    });
+
+    // When there is no more data to be consumed from the stream
+    blobWriter.end(req.file.buffer);
+  }
+);
+
 module.exports = router;
